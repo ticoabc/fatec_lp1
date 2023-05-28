bf193f3389e2aba9ceff9024a1b4eeadd07278e4
Tiago de Freitas <tiagoabc@gmail.com>
2023-05-28 20:09:03 -0300

    Atualizado o Package 'executa' 	Atualizado indices no menu "switch case" com os exercícios Atualizado o Package 'diversos' 	Atualizado a seguintes classes 		DiasdaSamana Atualizado o Package 'enums' 	Atualizado a seguinte classe 		Dias

--- LinguagemPrograma/src/diversos/DiasdaSemana.java
+++ LinguagemPrograma/src/diversos/DiasdaSemana.java
@@ -6,14 +6,62 @@ import java.util.Scanner;
 /* @author Tiago de Freitas */
 public class DiasdaSemana {
     
-     Dias dia;   
+    private Dias dia;
 
-//Método leitura
+    public String getDia() {
+        return dia.name();
+    }
+
+    public void setDia(String dia) {
+        this.dia = Dias.valueOf(dia);
+    }
+    
+    //Método leitura
     public void leitura() {
         Scanner leitor = new Scanner(System.in);
-        System.out.print("\n\tExercicios utilizando ENUMS\n");
-        System.out.print("\n\t>>>Dia da Semana<<<");
         
-        System.out.print("\n\tDigite um número entre 1 - 7: ");
+        int item = 0;
+        while (true){
+        System.out.print("\n\t\tExercicios utilizando ENUMS");
+        System.out.print("\n\t\t\t>>>Dia da Semana<<<");
+        System.out.print("\n\t\t\tDigite um número entre 1 - 7"
+                       + "\n\t\t\t0 - Finalizar");
+        System.out.print("\n\t\t\tDigite a operação desejada: ");
+        item = leitor.nextInt();
+        
+            switch (item){
+                case 1:
+                System.out.print("\t\t\t" + dia.DOMINGO + "\n");
+                break;
+
+                case 2:
+                System.out.print("\t\t\t" + dia.SEGUNDA + "\n");
+                break;
+
+                case 3:
+                System.out.print("\t\t\t" + dia.TERCA + "\n");
+                break;
+
+                case 4:
+                System.out.print("\t\t\t" + dia.QUARTA + "\n");
+                break;
+
+                case 5:
+                System.out.print("\t\t\t" + dia.QUINTA + "\n");
+                break;
+
+                case 6:
+                System.out.print("\t\t\t" + dia.SEXTA + "\n");
+                break;
+
+                case 7:
+                System.out.print("\t\t\t" + dia.SABADO + "\n");
+                break;
+
+                case 0:
+                System.exit(0);
+                break;
+            }
+        }
     }
 }
--- LinguagemPrograma/src/enums/Dias.java
+++ LinguagemPrograma/src/enums/Dias.java
@@ -3,17 +3,11 @@ package enums;
 /* @author Tiago de Freitas */
 public enum Dias {
     
-    DOMINGO(1), 
-    SEGUNDA(2), 
-    TERCA(3), 
-    QUARTA(4), 
-    QUINTA(5), 
-    SEXTA(6), 
-    SABADO(7);
-    
-    int valorDia;
-    
-    Dias(int dia){
-        valorDia=dia;
-    }
+    DOMINGO,
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO
 }
